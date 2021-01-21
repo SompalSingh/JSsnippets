2f5fc2cab353715342847fa5fb027ce76eef6354
sompal singh <sompal981@gmail.com>
2021-01-21 18:02:04 +0530

    Progree Steps

--- Progress Steps/assets/js/text.js
+++
@@ -1,6 +0,0 @@
-// var test = document.getElementById('prrogress');
-// const abc = document.getElementById('next');
-// test.style.width = 30
-// abc.addEventListener('click', ()=>{
-//     console.log(progress)
-// })
