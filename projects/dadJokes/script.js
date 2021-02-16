293d9cf2d76a1d5d4600e9e75548224d7a3f36f2
sompal singh <sompal981@gmail.com>
2021-02-16 18:43:18 +0530

    Dad Jokes

---
+++ projects/dadJokes/script.js
@@ -0,0 +1,17 @@
+const jokesEl = document.getElementById('jokes')
+const btn = document.getElementById('joksbtn');
+ btn.addEventListener('click', newjokes);
+
+newjokes()
+function newjokes(){
+    const config = {
+        headers:{
+            Accept:'application/json',
+        },
+    }
+    fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+        jokesEl.innerHTML = data.joke   
+    });
+}
